5ae14c2ce15f32af61058a13a030844cec1c01aa
maicol <maicol.matteoli@gmail.com>
2021-07-06 09:34:03 +0200

    add : jumbo

--- cli-first/src/App.vue
+++ cli-first/src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <Hero />
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <Title 
       titolo="La mia primissima app in Vue CLI(che emozione vedere che funziona)" 
-      titolo2="sono un h2 :)"
+      titolo2="sono un h2 :D"
     />
   </div>
 </template>
@@ -13,12 +14,13 @@
 //ogni import deve avere un export 
 // import HelloWorld from './components/HelloWorld.vue' 
 import Title from './components/Title.vue'
-
+import Hero from './components/Hero.vue'
 export default { //questo export corrisponde a import in main.js riga 2
   name: 'App',
   components: {
     // HelloWorld,
-    Title 
+    Title,
+    Hero 
   }
 }
 </script>
@@ -31,5 +33,8 @@ export default { //questo export corrisponde a import in main.js riga 2
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  /*toglie i margini orizz. ma c'è comunque uno spazio bianco  */
+  margin-left: 0;
+  margin-right: 0;
 }
 </style>
---
+++ cli-first/src/components/Hero.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="jumbo">
+        <!-- sono un jumbo -->
+        <div class="img_wrap">
+            <img src="../assets/jumboImg.jpg" alt="img jumbo">
+            <p>Sono un Jumbotron!!!!! E i nani sono stati catturati!</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Hero',
+        props:{
+            //nomeVariabile : tipo(string,bool,int,float ....)
+        },
+    }
+</script>
+
+<style scoped>
+    .jumbo{
+        /* background-color: lightblue; */
+        height: 600px;
+
+    }
+    .img_wrap{
+        position: relative;
+        height: 100%;
+    }
+    img{
+        height: 100%;
+    }
+    p{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50% , -50%);
+        color: white;
+        font-size: 3rem;
+    }
+</style>
